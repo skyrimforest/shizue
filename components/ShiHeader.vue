63c6ca89c49552c5b10a0cbbb56a076e1f252326
skyrimforest <835766751@qq.com>
2024-10-14 00:20:09 +0800

    fix:have a try

--- components/ShiHeader.vue
+++ components/ShiHeader.vue
@@ -1,84 +1,63 @@
 <template>
   <div id="outer-box">
     <div id="head-box">
-    <div id="left-box">
-      <img id="left-image" src="/img/dongban.png"/>
-    </div>
-
-    <div id="right-box">
-      <div id="right-upper-box">
-        <div id="info-box">
-          <div id="name">
-            Shizue      静江
-          </div>
-          <div class="row">
-            Male丨2001丨Master's Degree
-          </div>
-          <div class="row">
-            ENFP丨Libra
-          </div>
-          <div class="row">
-            NanJing University丨DisLab
-          </div>
-          <div class="row">
-          </div>
-          <div class="row">
-          </div>
-          <div class="row">
-            Contact me by: hitskyrim@qq.com
-          </div>
-        </div>
-
-        <div id="right-image-box">
-          <img id="right-image" src="/img/nju.png"/>
-        </div>
+      <div id="left-box">
+        <NuxtImg id="left-image" src="/img/dongban.png" />
       </div>
-      <div id="right-down-box">
-        <table >
-          <tbody>
-            <tr>
-              <td class="cell">
-                <NuxtLink to="/posts/1">
-                 <div class="row">
-                  Biology
-                 </div> 
-                </NuxtLink>
-              </td>
-              <td class="split"></td>
 
-              <td class="cell">
-                <NuxtLink to="/posts/1">
-                 <div class="row">
-                  Education
-                 </div> 
-                </NuxtLink>
-              </td>
-              <td class="split"></td>
+      <div id="right-box">
+        <div id="right-upper-box">
+          <div id="info-box">
+            <div id="name">Shizue 静江</div>
+            <div class="row">Male丨2001丨Master's Degree</div>
+            <div class="row">ENFP丨Libra</div>
+            <div class="row">NanJing University丨DisLab</div>
+            <div class="row"></div>
+            <div class="row"></div>
+            <div class="row">Contact me by: hitskyrim@qq.com</div>
+          </div>
 
-              <td class="cell">
-                <NuxtLink to="/posts/1">
-                 <div class="row">
-                  Honors
-                 </div> 
-                </NuxtLink>
-              </td>
-              <td class="split"></td>
+          <div id="right-image-box">
+            <NuxtImg id="right-image" src="/img/nju.png" />
+          </div>
+        </div>
+        <div id="right-down-box">
+          <table>
+            <tbody>
+              <tr>
+                <td class="cell">
+                  <NuxtLink to="/posts/1">
+                    <div class="row">Biology</div>
+                  </NuxtLink>
+                </td>
+                <td class="split"></td>
 
-              <td class="cell">
-                <NuxtLink to="/posts/1">
-                 <div class="row">
-                  Interests
-                 </div> 
-                </NuxtLink>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+                <td class="cell">
+                  <NuxtLink to="/posts/1">
+                    <div class="row">Education</div>
+                  </NuxtLink>
+                </td>
+                <td class="split"></td>
+
+                <td class="cell">
+                  <NuxtLink to="/posts/1">
+                    <div class="row">Honors</div>
+                  </NuxtLink>
+                </td>
+                <td class="split"></td>
+
+                <td class="cell">
+                  <NuxtLink to="/posts/1">
+                    <div class="row">Interests</div>
+                  </NuxtLink>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
-  </div>
-
 </template>
 
 <style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  modules: ["@nuxt/image"],
+  experimental: {
+    payloadExtraction: false,
+  },
   app: {
     pageTransition: { name: "page", mode: "out-in" },
+    baseURL: "/shizue.github.io/",
+    buildAssetsDir: "nuxt_assets",
   },
   css: ["@/assets/css/Global.css"],
 });
